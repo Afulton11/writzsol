2d818931839372a4d82ee3106e288cb3c9a71aa3
Andrew Fulton <afulton20@gmail.com>
2020-12-07 12:51:36 -0600

    Clean up use of ApolloClient
    * removed all apollo-
    * packages for the client, installed "@apollo/client"

--- pages/user/[title].js
+++ pages/user/[title].js
@@ -1,6 +1,14 @@
 import React from 'react'
-
 import { Heading, Flex, ButtonGroup, Button } from '@chakra-ui/core'
+import { initApolloClient } from '../../lib/graphql/apollo-client'
+
+export async function getStaticProps(context) {
+  const apolloClient = initApolloClient()
+
+  return {
+    props: {},
+  }
+}
 
 export default function Home() {
   return (
