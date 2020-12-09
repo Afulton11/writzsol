d141dd866b154a32b03d0f18adced6371e7fda62
Andrew Fulton <afulton20@gmail.com>
2020-12-09 11:37:02 -0600

    [Fix] Use BASE_URL to generate GQL Isomorphic link (#44)
    * Fixes #37

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ module.exports = {
     }
 
     if (process.env.APP_ENV && process.env.APP_ENV === 'LOCALHOST') {
-      config.devtool = 'cheap-module-eval-source-map'
+      config.devtool = 'eval-source-map'
     }
 
     // enable top-level await, see pages/api/gql/index.ts for usecase.
